aeab82144b3c300d0a9c1cb139ee2defbb88f708
Deirdre Saoirse Moen <deirdre@deirdre.net>
2022-06-18 23:22:51 -0700

    1. Update Package.swift to swift-tools-version 5.5. 2. Added explicit macOS(.v12) version. 3. Updated Publish to current version.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TwitterPublishPlugin",
+	platforms: [
+		.macOS(.v12),
+	],
     products: [
         .library(
             name: "TwitterPublishPlugin",
@@ -12,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0"),
+		.package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
     ],
     targets: [
         .target(
